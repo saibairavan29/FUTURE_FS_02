0d3ea4e88c731992d59b37b792d5d748e321ab00
Sai Bairavan <saibairavan29@gmail.com>
2025-12-05 20:46:06 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+// src/index.js  (or src/main.jsx)
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // needed for dropdowns & navbar toggler
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { BrowserRouter } from "react-router-dom";
+
+const basename = process.env.PUBLIC_URL || "/";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter basename={basename}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
